e41912541e58464f69f50912aa4e670577545e02
DarkRazec <darkrazec@yahoo.com>
2024-02-12 01:58:35 +0300

    tests for new features

--- tests/test_Category.py
+++ tests/test_Category.py
@@ -3,30 +3,27 @@ from src.modules.Category import Category
 from src.modules.Product import Product
 
 
-@pytest.fixture
-def some_category():
-    return Category('Containers', 'This is containers. They contain objects inside themselves.', [1, 2])
-
-
-@pytest.fixture
-def some_category_2():
-    return Category('Food', 'This is Food.', [Product('NOTBox', 'This is NOT the Box', 0, 0)])
-
-
-def test_getters(some_category):
-    assert some_category.get_name() == 'Containers'
-    assert some_category.get_desc() == 'This is containers. They contain objects inside themselves.'
-    assert type(some_category.get_products()) == list
-
-
-def test_products(some_category_2):
-    assert some_category_2.products == ['NOTBox, 0 руб. Остаток: 0 шт.']
+def test_getters():
+    food = Category('Food', 'This is Food.', [Product('NOTNOTBox', 'This is NOTNOT the Box', 0, 0)])
+    assert food.get_name() == 'Food'
+    assert food.get_desc() == 'This is Food.'
+    assert food.get_products()[0].get_name() == 'NOTNOTBox'
+    assert food.products == ['NOTNOTBox, 0 руб. Остаток: 0 шт.']
 
 
 def test_products_amount():
-    assert Category.products_amount == 3
+    assert Category.get_products_amount() == 1
+
+
+def test_add_products(monkeypatch):
+    new_cat = Category('new', 'new', [Product('Apple', 'This is Apple', 0, 0)])
+    new_cat.add_products(Product('Box', 'This is the Box', 1, 1))
+    assert Category.get_products_amount() == 3
+    assert new_cat.products == ['Apple, 0 руб. Остаток: 0 шт.', 'Box, 1 руб. Остаток: 1 шт.']
+    monkeypatch.setattr('builtins.input', lambda _: "y")
+    new_cat.add_products(Product('Box', 'This is the Box', 2, 2))
+    assert new_cat.products == ['Apple, 0 руб. Остаток: 0 шт.', 'Box, 2 руб. Остаток: 3 шт.']
 
 
 def test_categories_amount():
-    assert Category.category_amount == 2
-
+    assert Category.get_category_amount() == 2
--- tests/test_Product.py
+++ tests/test_Product.py
@@ -10,7 +10,7 @@ def some_product():
 def test_getters(some_product):  # actually, this is __init__ parameters, so I made them in one test
     assert some_product.get_name() == 'Box'
     assert some_product.get_desc() == 'This is the Box'
-    assert some_product.get_count() == 5
+    assert some_product.count == 5
     assert some_product.price == 1.1
 
 
@@ -25,17 +25,23 @@ def test_set_price(some_product, monkeypatch):
     assert some_product.price == 0
 
 
+def test_set_count(some_product, monkeypatch):
+    some_product.count = 2
+    assert some_product.count == 2
+
+
 def test_delete_price(some_product):
     del some_product.price
     assert some_product.price is None
 
 
+def test_delete_count(some_product):
+    del some_product.count
+    assert some_product.count is None
+
+
 def test_make_product():
     prod = Product('NOTBox', 'This is NOT the Box', 0, 4)
-    prod.make_product('NOTBox', 'This is NOT the Box', 2, 3)
     assert prod.get_name() == 'NOTBox'
-    assert prod.price, prod.get_count() == (2, 7)
-    new_prod = prod.make_product('New Product', 'This is the new Product', 1, 1)
-    new_prod.make_product('New Product', 'This is the new Product', 0, 2)
-    assert new_prod.price, new_prod.get_count() == (1, 3)
-
+    prod.make_product('NOTBox', 'This is NOT the Box', 2, 3)
+    assert prod.price, prod.count == (2, 7)
