6b7a1e9032e24794f32bcb94f9f30a97ee3c0fed
DarkRazec <darkrazec@yahoo.com>
2024-02-20 01:50:08 +0300

    add Grass class

--- src/modules/Grass.py
+++ src/modules/Grass.py
@@ -3,3 +3,7 @@ from src.modules.Product import Product
 
 class Grass(Product):
     """Класс для абстракции 'Газонная трава'"""
+    def __init__(self, name, desc, price, count, color: str, made_in: str, germination: int):
+        super().__init__(name, desc, price, count, color)
+        self.__made_in = made_in
+        self.__germination = germination
