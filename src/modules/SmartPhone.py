a7ef5179e3b81f7d5c1e11530464492a81648afc
DarkRazec <darkrazec@yahoo.com>
2024-02-20 01:49:49 +0300

    add SmartPhone and Grass classes

--- src/modules/SmartPhone.py
+++ src/modules/SmartPhone.py
@@ -3,3 +3,8 @@ from src.modules.Product import Product
 
 class SmartPhone(Product):
     """Класс для абстракции 'Смартфон'"""
+    def __init__(self, name, desc, price, count, color: str, performance: float, model: str, memory: float):
+        super().__init__(name, desc, price, count, color)
+        self.__performance = performance
+        self.__model = model
+        self.__memory = memory
