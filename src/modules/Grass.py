4b3302bad0098fc664c02ce8f0880a0b225f9144
DarkRazec <darkrazec@yahoo.com>
2024-02-25 00:01:30 +0300

    add abstract class for all prodict-like classes

--- src/modules/Grass.py
+++ src/modules/Grass.py
@@ -1,8 +1,12 @@
+from src.modules.AbstractProduct import AbstractProduct
 from src.modules.Product import Product
 
 
-class Grass(Product):
+class Grass(Product, AbstractProduct):
     """Класс для абстракции 'Газонная трава'"""
+    made_in: str
+    germination: int
+
     def __init__(self, name, desc, price, count, color: str, made_in: str, germination: int):
         super().__init__(name, desc, price, count, color)
         self.__made_in = made_in
--- src/modules/SmartPhone.py
+++ src/modules/SmartPhone.py
@@ -1,8 +1,13 @@
 from src.modules.Product import Product
+from src.modules.AbstractProduct import AbstractProduct
 
 
-class SmartPhone(Product):
+class SmartPhone(Product, AbstractProduct):
     """Класс для абстракции 'Смартфон'"""
+    performance: float
+    model: str
+    memory: float
+
     def __init__(self, name, desc, price, count, color: str, performance: float, model: str, memory: float):
         super().__init__(name, desc, price, count, color)
         self.__performance = performance
